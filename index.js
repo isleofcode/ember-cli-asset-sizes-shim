90a6814f297638d6335a3fca1b51f15e8bd0b855
Chris Thoburn <runspired@gmail.com>
2016-04-28 18:36:23 -0500

    stashing

--- index.js
+++ index.js
@@ -4,126 +4,75 @@ var chalk = require('chalk');
 var analyze = require('./lib/helpers/analyze-path');
 var logFile = require('./lib/helpers/log-file-stats');
 var path = require('path');
+var BuildCommand = require('./lib/commands/build');
+var analyzeAddon = require('./lib/helpers/analyze-addon');
 
 module.exports = {
   name: 'ember-cli-asset-sizes-shim',
 
+
+  /*
+    Alert the User to improper installs.
+   */
   included: function(app) {
-
     if (!app._wasShimmedForStats) {
-      console.log(chalk.red('You should shim your app'));
+      console.log(
+        chalk.yellow('[WARNING] Missing asset-sizes shim!\n') +
+        chalk.grey(
+            '\tThe addon ember-cli-asset-sizes-shim requires you to shim EmberApp.\n' +
+            '\n\tPlace the following in your ember-cli-build.js file.\n\n') +
+        chalk.green('var shim = require(\'ember-cli-asset-sizes-shim/lib/shim\');\n\nshim(EmberApp);')
+      );
     }
-
   },
 
+
+  /*
+    We override default commands to add tracing flags and expose them to the environment
+   */
+  includedCommands: function() {
+    return {
+      build: BuildCommand
+    }
+  },
+
+
+  /*
+   Make sure this returns false before publishing
+   */
+  isDevelopingAddon: function() {
+    return true;
+  },
+
+
+  /*
+    We use output ready to do the analysis so that we have access to the final build as well.
+   */
   outputReady: function(result) {
+    // bail if we have no shim or process flags
+    if (!this.app._wasShimmedForStats || !process._flags) {
+      return;
+    }
+
+    var shouldAnalyzeAssests = process._flags.assetSizes;
+    var assetToTrace = process._flags.traceAsset;
+
+    // bail if the user didn't wan't us to log or trace anything
+    if (!shouldAnalyzeAssests && !assetToTrace) {
+      return;
+    }
 
     // asset analytics
     var AssetCache = this.app._assetAnalytics;
     var root = this.project.root;
 
-    console.log(chalk.white('\nAddon Analytics\n=================='));
+    console.log(chalk.white('\nAsset Analytics') + chalk.grey('\n––––––––––––––––––––––––––––'));
+
     Object.keys(AssetCache).forEach(function(addon) {
-      var msgs = [];
-      var Asset = AssetCache[addon];
-      var sizes;
-      var info;
-
-      // app code
-      if (Asset.app) {
-        sizes = Asset.app.reduce(function (chain, info) {
-          chain.total += info.stats.size;
-          chain.gzipped += info.gzipped.length;
-          chain.uglified += info.uglified.length;
-          chain.both += info.both.length;
-
-          return chain;
-        }, {total: 0, gzipped: 0, uglified: 0, both: 0});
-        info = {
-          isJS: true,
-          realFilePath: 'App Modules',
-          stats: {size: sizes.total},
-          gzipped: {length: sizes.gzipped},
-          uglified: {length: sizes.uglified},
-          both: {length: sizes.both}
-        };
-        if (sizes.total) {
-          msgs.push(logFile(info, true));
-        }
-      }
-
-      // addon code
-      if (Asset.addon) {
-        sizes = Asset.addon.reduce(function (chain, info) {
-          chain.total += info.stats.size;
-          chain.gzipped += info.gzipped.length;
-          chain.uglified += info.uglified.length;
-          chain.both += info.both.length;
-
-          return chain;
-        }, {total: 0, gzipped: 0, uglified: 0, both: 0});
-        info = {
-          isJS: true,
-          realFilePath: 'Addon Modules',
-          stats: {size: sizes.total},
-          gzipped: {length: sizes.gzipped},
-          uglified: {length: sizes.uglified},
-          both: {length: sizes.both}
-        };
-        if (sizes.total) {
-          msgs.push(logFile(info, true));
-        }
-      }
-
-      // vendor / imported code (app.import)
-      if (Asset.imports) {
-        var imports = [];
-
-        Asset.imports.forEach(function(item) {
-          analyze(root, path.join(root, item.path), function(info) {
-            imports.push(info);
-          });
-        });
-
-        sizes = imports.reduce(function(chain, info) {
-          chain.total += info.stats.size;
-          chain.gzipped += info.gzipped.length;
-          chain.uglified += info.uglified.length;
-          chain.both += info.both.length;
-
-          return chain;
-        }, { total: 0, gzipped: 0, uglified: 0, both: 0 });
-        info = {
-          isJS: true,
-          realFilePath: 'Vendor Imports',
-          stats: { size: sizes.total },
-          gzipped: { length: sizes.gzipped },
-          uglified: { length: sizes.uglified },
-          both: { length: sizes.both }
-        };
-        if (sizes.total) {
-          msgs.push(logFile(info, true));
-          imports.forEach(function(info) {
-            if (info.stats.size) {
-              var m = logFile(info, true, '  ');
-              msgs.push(chalk.grey(chalk.stripColor(m)));
-            }
-          });
-
-        }
-      }
-
-      // templates
-
-      // styles
-
-      if (msgs.length) {
-        console.log(chalk.green(addon));
-        msgs.forEach(function(msg) {
-          console.log(msg);
-        });
-      }
-
+      analyzeAddon(addon, AssetCache[addon], {
+        shouldLog: addon === assetToTrace,
+        root: root
+      });
     });
 
     console.log(chalk.white('\nFinal Build Analytics\n=================='));
@@ -132,10 +81,6 @@ module.exports = {
     });
 
 
-  },
-
-  isDevelopingAddon: function() {
-    return true;
   }
 
 };
