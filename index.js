ae3e2279acffa951a765d3a73bfb7a0db41562d6
Chris Thoburn <runspired@gmail.com>
2016-04-29 10:24:40 -0500

    removes need to manually shim

--- index.js
+++ index.js
@@ -5,26 +5,10 @@ var analyze = require('./lib/helpers/analyze-path');
 var logFile = require('./lib/helpers/log-file-stats');
 var path = require('path');
 var BuildCommand = require('./lib/commands/build');
+var shim = require('./lib/shim');
 
 module.exports = {
   name: 'ember-cli-asset-sizes-shim',
-
-
-  /*
-    Alert the User to improper installs.
-   */
-  included: function(app) {
-    if (!app._wasShimmedForStats) {
-      console.log(
-        chalk.yellow('[WARNING] Missing asset-sizes shim!\n') +
-        chalk.grey(
-            '\tThe addon ember-cli-asset-sizes-shim requires you to shim EmberApp.\n' +
-            '\n\tPlace the following in your ember-cli-build.js file.\n\n') +
-        chalk.green('var shim = require(\'ember-cli-asset-sizes-shim/lib/shim\');\n\nshim(EmberApp);')
-      );
-    }
-  },
-
 
   /*
     We override default commands to add tracing flags and expose them to the environment
@@ -48,16 +32,11 @@ module.exports = {
     We use output ready to do the analysis so that we have access to the final build as well.
    */
   outputReady: function(result) {
-
-    // bail out if we have no shim
-    if (!this.app._wasShimmedForStats) {
-      return;
-    }
-
     var assetCache = this.app.__cacheForAssetStats;
 
     // bail if the user didn't wan't us to log or trace anything
     if (!assetCache.isActive) {
+      console.log('bailing because inactive');
       return;
     }
 
@@ -92,6 +71,17 @@ module.exports = {
 
     }
 
+  },
+
+  init: function() {
+    if (this._super && this._super.apply) {
+      this._super.apply(this, arguments);
+    }
+
+    var pathToApp = path.join(this.project.root, 'node_modules/ember-cli/lib/broccoli/ember-app');
+    var EmberApp = require(pathToApp);
+
+    shim(EmberApp);
   }
 
 };
